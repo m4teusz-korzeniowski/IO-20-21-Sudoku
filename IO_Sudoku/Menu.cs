3815f15838cffe836670e04bdb6b7856c9b34311
Mateusz Korzeniowski <m4teusz.korzeniowski@gmail.com>
2021-04-02 07:23:48 +0200

    Przemieszczanie sie pomiedzy oknami, bazowy layout

---
+++ IO_Sudoku/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IO_Sudoku
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var userChoice = new UserChoice();
+            userChoice.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var board = new MainForm();
+            board.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ IO_Sudoku/UserChoice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IO_Sudoku
+{
+    public partial class UserChoice : Form
+    {
+        public UserChoice()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var menu = new Menu();
+            //menu.Location = this.Location;
+            menu.Show();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
