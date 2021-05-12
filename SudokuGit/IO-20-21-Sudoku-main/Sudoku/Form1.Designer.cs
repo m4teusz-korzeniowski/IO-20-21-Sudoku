3d77333d3e5602c20438a8e504a7c5191712ce6c
FilipOwcz <80968949+FilipOwcz@users.noreply.github.com>
2021-05-12 13:42:44 +0200

    Add files via upload

---
+++ SudokuGit/IO-20-21-Sudoku-main/Sudoku/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace Sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.NowaGra = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Sprawdz = new System.Windows.Forms.Button();
+            this.Zapisz = new System.Windows.Forms.Button();
+            this.Wyszysc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(737, 672);
+            this.panel1.TabIndex = 0;
+            // 
+            // NowaGra
+            // 
+            this.NowaGra.Location = new System.Drawing.Point(12, 690);
+            this.NowaGra.Name = "NowaGra";
+            this.NowaGra.Size = new System.Drawing.Size(151, 92);
+            this.NowaGra.TabIndex = 1;
+            this.NowaGra.Text = "Nowa Gra";
+            this.NowaGra.UseVisualStyleBackColor = true;
+            this.NowaGra.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(174, 719);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(65, 21);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Łatwy";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(174, 746);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(70, 21);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Średni";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(174, 773);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(74, 21);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Trudny";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(169, 690);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 29);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Poziom";
+            // 
+            // Sprawdz
+            // 
+            this.Sprawdz.Location = new System.Drawing.Point(263, 690);
+            this.Sprawdz.Name = "Sprawdz";
+            this.Sprawdz.Size = new System.Drawing.Size(151, 92);
+            this.Sprawdz.TabIndex = 6;
+            this.Sprawdz.Text = "Sprawdź";
+            this.Sprawdz.UseVisualStyleBackColor = true;
+            this.Sprawdz.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Zapisz
+            // 
+            this.Zapisz.Location = new System.Drawing.Point(420, 690);
+            this.Zapisz.Name = "Zapisz";
+            this.Zapisz.Size = new System.Drawing.Size(151, 92);
+            this.Zapisz.TabIndex = 7;
+            this.Zapisz.Text = "Zapisz";
+            this.Zapisz.UseVisualStyleBackColor = true;
+            // 
+            // Wyszysc
+            // 
+            this.Wyszysc.Location = new System.Drawing.Point(577, 690);
+            this.Wyszysc.Name = "Wyszysc";
+            this.Wyszysc.Size = new System.Drawing.Size(151, 92);
+            this.Wyszysc.TabIndex = 8;
+            this.Wyszysc.Text = "Wyczyść";
+            this.Wyszysc.UseVisualStyleBackColor = true;
+            this.Wyszysc.Click += new System.EventHandler(this.Wyszysc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 816);
+            this.Controls.Add(this.Wyszysc);
+            this.Controls.Add(this.Zapisz);
+            this.Controls.Add(this.Sprawdz);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.NowaGra);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sudoku";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button NowaGra;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Sprawdz;
+        private System.Windows.Forms.Button Zapisz;
+        private System.Windows.Forms.Button Wyszysc;
+    }
+}
+
